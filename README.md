71465cfcdb2e439fb821b1513515a0876009c2ee
stephaniecoleman <sproutwingsandfly@gmail.com>
2015-05-01 11:52:45 -0400

    switch from grunt to learn.

--- README.md
+++ README.md
@@ -4,17 +4,10 @@ tags: math, triangles
 ---
 
 # Pythagorean Triplet
-## Getting Started
-
-Run `npm install`
 
 ## Tests
 
 Review the `triplet.js` file inside the `lib` directory, and then the `triplet.spec.js` file inside the `spec` directory. Review each of the tests, and then start implementing your solutions to make each test pass in `triplet.js`.
-
-Run `grunt watch` then edit your files. Grunt will watch for changes. Grunt
-will only run tests that start with `it`. When you finish passing a test,
-remove the `x` from `xit` to convert it to a runnable test.
 
 ## Background
 
--- spec/triplet.spec.js
+++ spec/triplet.spec.js
@@ -1,7 +1,5 @@
 'use strict';
 
-var Triplet = require('../lib/triplet');
-
 describe('Triplet', function () {
   // http://en.wikipedia.org/wiki/Pythagorean_triple
 
@@ -9,19 +7,19 @@ describe('Triplet', function () {
     expect(new Triplet(3, 4, 5).sum()).toBe(12);
   });
 
-  xit('calculates the product', function () {
+  it('calculates the product', function () {
     expect(new Triplet(3, 4, 5).product()).toBe(60);
   });
 
-  xit('can recognize a pythagorean triplet', function () {
+  it('can recognize a pythagorean triplet', function () {
     expect(new Triplet(3, 4, 5).isPythagorean()).toBe(true);
   });
 
-  xit('can recognize a non pythagorean triplet', function () {
+  it('can recognize a non pythagorean triplet', function () {
     expect(new Triplet(5, 6, 7).isPythagorean()).not.toBe(true);
   });
 
-  xit('can make triplets up to 10', function () {
+  it('can make triplets up to 10', function () {
     var triplets = Triplet.where({ maxFactor: 10 });
     var products = triplets.sort().map( function (triplet) {
       return triplet.product();
@@ -29,7 +27,7 @@ describe('Triplet', function () {
     expect(products).toEqual([60, 480]);
   });
 
-  xit('can make triplets 11 through 20', function () {
+  it('can make triplets 11 through 20', function () {
     var triplets = Triplet.where({ minFactor: 11, maxFactor: 20 });
     var products = triplets.sort().map( function (triplet) {
       return triplet.product();
@@ -37,7 +35,7 @@ describe('Triplet', function () {
     expect(products).toEqual([3840]);
   });
 
-  xit('can filter on sum', function () {
+  it('can filter on sum', function () {
     var triplets = Triplet.where({ sum: 180, maxFactor: 100 });
     var products = triplets.sort().map( function (triplet) {
       return triplet.product();
